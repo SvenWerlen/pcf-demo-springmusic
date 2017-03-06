4de539b2355e0844bf809464ff02da02cb13d554
Sven Werlen <sven.werlen@desjardins.com>
2017-03-06 00:18:45 -0500

    ci: fix output

--- ci/scripts/build-et-test.sh
+++ ci/scripts/build-et-test.sh
@@ -4,11 +4,13 @@ set -e
 
 ROOT_FOLDER=$(pwd)
 APP_FOLDER=${ROOT_FOLDER}/springmusic
+OUT_FOLDER=${ROOT_FOLDER}/springmusic-app
 VERSION=$(date +"%Y%m%d%H%M%S")
 
 # Debug
 echo ROOT_FOLDER:${ROOT_FOLDER}
 echo APP_FOLDER:${APP_FOLDER}
+echo OUT_FOLDER:${OUT_FOLDER}
 echo VERSION:${VERSION}
 
 # Préparation
@@ -22,10 +24,10 @@ cd ${APP_FOLDER}
 
 # Création de l'archive
 cp ${APP_FOLDER}/build/libs/spring-music.jar ${APP_FOLDER}/manifest.yml ${ROOT_FOLDER}/archive
-tar -C ${ROOT_FOLDER}/archive -czvf ${ROOT_FOLDER}/springmusic-app-${VERSION}.tar.gz spring-music.jar manifest.yml
+tar -C ${ROOT_FOLDER}/archive -czvf ${OUT_FOLDER}/springmusic-app-${VERSION}.tar.gz spring-music.jar manifest.yml
 
 # Nettoyage des fichiers temporaires
 rm -rf ${ROOT_FOLDER}/{reports,test-results,archive}
 
 # Afficher md5sum
-md5sum ${ROOT_FOLDER}/springmusic-app-${VERSION}.tar.gz
+md5sum ${OUT_FOLDER}/springmusic-app-${VERSION}.tar.gz
