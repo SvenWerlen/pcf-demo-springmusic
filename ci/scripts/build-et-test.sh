e96e22b0a501eb94e34f8040e22cfe4639697bae
Sven Werlen <sven.werlen@desjardins.com>
2017-03-05 23:59:16 -0500

    ci: améliorations

--- ci/scripts/build-et-test.sh
+++ ci/scripts/build-et-test.sh
@@ -2,9 +2,13 @@
 
 set -e
 
-ROOT_FOLDER=$(pwd)/springmusic
+ROOT_FOLDER=$(pwd)
+APP_FOLDER=${ROOT_FOLDER}/springmusic
 VERSION=$(date +"%Y%m%d%H%M%S")
+
+# Debug
 echo ROOT_FOLDER:${ROOT_FOLDER}
+echo APP_FOLDER:${APP_FOLDER}
 echo VERSION:${VERSION}
 
 # Préparation
@@ -13,11 +17,11 @@ mkdir -p ${ROOT_FOLDER}/test-results
 mkdir -p ${ROOT_FOLDER}/archive
 
 # Build
-cd ${ROOT_FOLDER}
+cd ${APP_FOLDER}
 ./gradlew build
 
 # Création de l'archive
-cp ${ROOT_FOLDER}/build/libs/spring-music.jar ${ROOT_FOLDER}/manifest.yml ${ROOT_FOLDER}/archive
+cp ${APP_FOLDER}/build/libs/spring-music.jar ${APP_FOLDER}/manifest.yml ${ROOT_FOLDER}/archive
 tar -C ${ROOT_FOLDER}/archive -czvf ${ROOT_FOLDER}/springmusic-app-${VERSION}.tar.gz spring-music.jar manifest.yml
 
 # Nettoyage des fichiers temporaires
